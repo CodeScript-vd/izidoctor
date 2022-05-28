7720dcef6032e3cc780c572ea2bb9455349a999b
davidsilvaco <davidsc14@hotmail.com>
2022-05-28 12:11:05 -0500

    Add files via upload
    Actauliza Dao Onmenu

--- src/main/java/com/izidoctor/model/dao/MetodoCentroMedicoEspecialidadBeans.java
+++ src/main/java/com/izidoctor/model/dao/MetodoCentroMedicoEspecialidadBeans.java
@@ -16,31 +16,42 @@ import com.izidoctor.util.ConnectionDB;
 public class MetodoCentroMedicoEspecialidadBeans {
 
     @Inject
-    private CentroMedicoEspecialidadBeans cme;
+    private CentroMedicoEspecialidadBeans cme;    
     private ArrayList<SelectItem> cmbCME = null;
     
     
     public MetodoCentroMedicoEspecialidadBeans() {
     }
     //public ArrayList<CentroMedicoBeans> getListaCentroMedico()  {
-    public ArrayList<SelectItem> getCmbCME()  {
+    public void getCmbCME(int idCentroMedicoSucursal)  {
         if (cmbCME == null){
             cmbCME = new ArrayList<>();
             //ArrayList<CentroMedicoBeans> data=new ArrayList<>();
            //ArrayList<SelectItem> data=new ArrayList<>();
             ConnectionDB cn=new ConnectionDB();
             try {
+                System.out.println("llego idCentroMedicoSucursal "+idCentroMedicoSucursal);
                 Statement sql=cn.getConnection().createStatement();
-                ResultSet rs=(ResultSet) sql.executeQuery("Select idEspecialidad, nombre, idCentroMedicoSucursal from especialidadsucursal");
+                String cadena = "Select idEspecialidad, nombre, idCentroMedicoSucursal from especialidadsucursal where idCentroMedicoSucursal = "+idCentroMedicoSucursal;
+                ResultSet rs=(ResultSet) sql.executeQuery(cadena);
                 while(rs.next()){
                     //CentroMedicoBeans u=new CentroMedicoBeans(rs.getInt(1),rs.getString(2),                
                     SelectItem u=new SelectItem(rs.getInt(1),rs.getString(2));                
                     cmbCME.add(u);
                 }                      
             }catch(Exception e){ 
-
+                System.out.println("No Recupero Especialidad de Base Datos");
             }            
-        }
+        }    
+    }
+
+    public ArrayList<SelectItem> getCmbCME() {
         return cmbCME;
     }
+
+    public void setCmbCME(ArrayList<SelectItem> cmbCME) {
+        this.cmbCME = cmbCME;
+    }
+    
+    
 }
--- src/main/java/com/izidoctor/model/dao/MetodoCentroMedicoSucursalBeans.java
+++ src/main/java/com/izidoctor/model/dao/MetodoCentroMedicoSucursalBeans.java
@@ -18,33 +18,107 @@ public class MetodoCentroMedicoSucursalBeans {
 
    
     @Inject
-    private CentroMedicoSucursalBeans cenms;    
+    private CentroMedicoSucursalBeans cenms;
+    @Inject
+    private MetodoCentroMedicoEspecialidadBeans metodoCentroMedicoEspecialidadBeans;
     private ArrayList<SelectItem> cmbCMS = null;
+    private int idCentroMedicoSucursal = 0;
+
+
+    
     public MetodoCentroMedicoSucursalBeans() {
     }
 
     //public ArrayList<CentroMedicoBeans> getListaCentroMedico()  {
-    public ArrayList<SelectItem> getCmbCM()  {
+    public void getCargaCMS(int idCentroMedico)  {
         if (cmbCMS == null){
             cmbCMS = new ArrayList<>();
-            //ArrayList<CentroMedicoBeans> data=new ArrayList<>();
-           //ArrayList<SelectItem> data=new ArrayList<>();
+          
+            for (CentroMedicoSucursalBeans centroMedicoSucursalBeans : getSucursalxCentroMedico(idCentroMedico)) {
+                SelectItem u=new SelectItem(centroMedicoSucursalBeans.getIdCentroMedicoSucursal(),centroMedicoSucursalBeans.getReferencia());
+                cmbCMS.add(u);
+            }     
+        }        
+    }
+   
+    public ArrayList<CentroMedicoSucursalBeans> getCentroSucursalAll(){
+            ArrayList<CentroMedicoSucursalBeans> lst = new ArrayList<CentroMedicoSucursalBeans>();
             ConnectionDB cn=new ConnectionDB();
             try {
                 Statement sql=cn.getConnection().createStatement();
-                ResultSet rs=(ResultSet) sql.executeQuery("Select idCentroMedicoSucursal, referencia, idcentromedico from centromedicosucursal");
+                ResultSet rs=(ResultSet) sql.executeQuery("SELECT idCentroMedicoSucursal, referencia, idcentromedico from centromedicosucursal");
                 while(rs.next()){
-                    //CentroMedicoBeans u=new CentroMedicoBeans(rs.getInt(1),rs.getString(2),                
-                    SelectItem u=new SelectItem(rs.getInt(1),rs.getString(2));                
-                    cmbCMS.add(u);
+                    CentroMedicoSucursalBeans cmbs= new CentroMedicoSucursalBeans();
+                    cmbs.setIdCentroMedicoSucursal(rs.getInt(1));
+                    cmbs.setReferencia(rs.getString(2));
+                    cmbs.setIndicadorActivo(rs.getInt(3));
+                    
+                    lst.add(cmbs);
                 }      
                 
             }catch(Exception e){ 
 
             }
 
+        return lst;
+    }
+    
+    public ArrayList<CentroMedicoSucursalBeans> getSucursalxCentroMedico(int idCentroMedico)  {
+        
+            ArrayList<CentroMedicoSucursalBeans> lst = new ArrayList<CentroMedicoSucursalBeans>();
+            ConnectionDB cn=new ConnectionDB();
+            try {
+                Statement sql=cn.getConnection().createStatement();
+                String consulta = "Select idCentroMedicoSucursal, referencia, idcentromedico from centromedicosucursal where idcentromedico = "+idCentroMedico;
+                ResultSet rs=(ResultSet) sql.executeQuery(consulta);
+                while(rs.next()){
+                    CentroMedicoSucursalBeans cmbs= new CentroMedicoSucursalBeans();
+                    cmbs.setIdCentroMedicoSucursal(rs.getInt(1));
+                    cmbs.setReferencia(rs.getString(2));
+                    cmbs.setIndicadorActivo(rs.getInt(3));
+                    
+                    lst.add(cmbs);
+                }      
+                
+            }catch(Exception e){ 
+                System.out.println(e.getMessage());
+            }
+
+        return lst;
             
-        }
+           
+    }
+    
+    public void obtenerCentroMedicoSucursal(){
+         try {
+                metodoCentroMedicoEspecialidadBeans.getCmbCME(idCentroMedicoSucursal);
+                System.out.println("Entra CentroMedicoSucursal "+idCentroMedicoSucursal);
+            } catch (Exception e) {
+                System.out.println("no llego Especialidad "+idCentroMedicoSucursal);
+            }
+    }
+    public ArrayList<SelectItem> getCmbCMS() {
         return cmbCMS;
     }
+
+    public void setCmbCMS(ArrayList<SelectItem> cmbCMS) {
+        this.cmbCMS = cmbCMS;
+    }
+    
+        public MetodoCentroMedicoEspecialidadBeans getMetodoCentroMedicoEspecialidadBeans() {
+        return metodoCentroMedicoEspecialidadBeans;
+    }
+
+    public void setMetodoCentroMedicoEspecialidadBeans(MetodoCentroMedicoEspecialidadBeans metodoCentroMedicoEspecialidadBeans) {
+        this.metodoCentroMedicoEspecialidadBeans = metodoCentroMedicoEspecialidadBeans;
+    }
+
+    public int getIdCentroMedicoSucursal() {
+        return idCentroMedicoSucursal;
+    }
+
+    public void setIdCentroMedicoSucursal(int idCentroMedicoSucursal) {
+        this.idCentroMedicoSucursal = idCentroMedicoSucursal;
+    }
+    
 }
